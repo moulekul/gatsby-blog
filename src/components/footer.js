7d45e35bc492d17208b2c5947abbae1f1247d6c6
Luke Moule <lukemoule20@gmail.com>
2023-06-07 22:26:52 +0100

    Create components

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Created by Luke Moule, &copy; 2023</p>
+        </footer>
+    )
+}
+
+export default Footer
