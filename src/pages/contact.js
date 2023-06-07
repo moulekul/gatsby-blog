3f7352ae648ff0aaf52ce22b4eccca43a0624b31
Luke Moule <lukemoule20@gmail.com>
2023-06-07 22:27:39 +0100

    Update pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import Layout from '../components/layout'
+
 const ContactPage = () => {
     return (
         <div>
+            <Layout>
             <h1>Contact</h1>
-            <p>The best way to reach me is via <a href="https://www.linkedin.com/in/lukemoule/">LinkedIn.</a></p>
+            <p>
+                The best way to reach me is via <a href="https://www.linkedin.com/in/lukemoule/">LinkedIn.</a>
+            </p>
+            </Layout>
         </div>
     )
 }
