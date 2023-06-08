998aab28a8b6dd2761e474d4d8619e4572c6990b
Luke Moule <lukemoule20@gmail.com>
2023-06-08 22:05:49 +0100

    Update components

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const Footer = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    author
+                }
+            }
+        }
+    `)
+
+
     return (
         <footer>
-            <p>Created by Luke Moule, &copy; 2023</p>
+            <p>Created by {data.site.siteMetadata.author}, &copy; 2023</p>
         </footer>
     )
 }
