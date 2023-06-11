0d946246060d86fdecc4b1b3a2e5553ebe95d8e1
Luke Moule <lukemoule20@gmail.com>
2023-06-11 11:21:11 +0100

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,8 @@ const AboutPage = () => {
         <div>
             <Layout>
             <h1>About Me</h1>
-            <p>I'm currently teaching myself how to code.</p>
+            <p>I’m just a dude who decided to make a website to document his life.
+               I’m not all that special, successful, interesting, or brilliant, though others would say otherwise.</p>
             <p><Link to="/contact">Want to work with me? Reach out.</Link></p>
             </Layout>
         </div>
